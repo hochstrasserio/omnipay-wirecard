806293eb5f0b5ae663518d9518c3e7f1a1888d7a
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2014-08-06 15:00:50 +0200

    Add factory for DataStorageRequest and CompletePurchaseRequest

--- src/Omnipay/Wirecard/Gateway.php
+++ src/Omnipay/Wirecard/Gateway.php
@@ -56,9 +56,27 @@ class Gateway extends AbstractGateway
     	return $this->setParameter('paymentType', $value);
     }
 
+    public function dataStorage(array $parameters = array())
+    {
+        return $this->createRequest('\Omnipay\Wirecard\Message\DataStorageRequest', $parameters);
+    }
+
     public function purchase(array $parameters = array())
     {
         return $this->createRequest('\Omnipay\Wirecard\Message\PurchaseRequest', $parameters);
     }
-    
+
+    /**
+     * Just a dummy request for now, which will be always successful.
+     *
+     * Will have to figure out how to handle this, since Wirecard transfers any payment info
+     * trough a separate server to server request, and not via the callback URLs.
+     *
+     * @param array $paramters Message parameters
+     * @return Omnipay\Wirecard\Message\CompletePurchaseRequest
+     */
+    public function completePurchase(array $parameters = array())
+    {
+        return $this->createRequest('\Omnipay\Wirecard\Message\CompletePurchaseRequest', $parameters);
+    }
 }
